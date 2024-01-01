d45ff7621e86fcc407f835310ae651ce4fa00e21
Fardin7864 <fardinhasan1888@gmail.com>
2024-01-01 17:29:20 +0600

    set home page gradient

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <LinearGradient  colors={['#3697e5', '#3679e0']} start={{y:1,x:1}} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <LinearGradient
-      // Button Linear Gradient
-      colors={['#4c669f', '#3b5998', '#192f6a']}
-      >
-      <Text>Sign in with Facebook</Text>
-    </LinearGradient>
+  <LinearGradient  colors={['#3697e5', '#3679e0']} start={{y:1,x:0.9}} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
   </LinearGradient>
   );
 }
